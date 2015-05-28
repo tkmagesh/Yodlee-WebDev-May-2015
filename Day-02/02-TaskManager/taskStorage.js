f2db6bfbe3114c513a6c7f7dc3c396306452d904
tkmagesh <tkmagesh77@gmail.com>
2015-05-28 17:21:29 +0530

    Custom Events

--- Day-02/02-TaskManager/taskStorage.js
+++ Day-02/02-TaskManager/taskStorage.js
@@ -1,39 +1,48 @@
 var taskStorage = (function(){
+    var addedCallbacks = [];
+    function getAllTasks(){
 
-            function getAllTasks(){
-                var result = [];
-                for(var i=0; i<localStorage.length; i++){
-                    var taskId = localStorage.key(i);
-                    var taskDataAsString = localStorage.getItem(taskId);
-                    var obj = JSON.parse(taskDataAsString);
-                    result.push(obj);
-                }
-                return result;
+        for(var i=0; i<localStorage.length; i++){
+            var taskId = localStorage.key(i);
+            var taskDataAsString = localStorage.getItem(taskId);
+            var obj = JSON.parse(taskDataAsString);
+            for(var i=0; i<addedCallbacks.length; i++){
+                var callback = addedCallbacks[i];
+                callback(obj);
             }
-            function removeTask(key){
-                localStorage.removeItem(key);
-            }
-            function addTask(taskName){
-                var taskId = Date.now().toString();
-                var newTask = {
-                    id : taskId,
-                    name : taskName,
-                    isCompleted : false
-                };
-                localStorage.setItem(taskId, JSON.stringify(newTask));
-                return newTask;
-            }
-            function toggleTask(id){
-                var task = JSON.parse(localStorage.getItem(id));
-                task.isCompleted = !task.isCompleted;
-                localStorage.setItem(id, JSON.stringify(task));
-            }
+        }
 
-            return{
-                getAll : getAllTasks,
-                remove : removeTask,
-                add : addTask,
-                toggle : toggleTask
-            };
+    }
+    function removeTask(key){
+        localStorage.removeItem(key);
+    }
+    function addTask(taskName){
+        var taskId = Date.now().toString();
+        var newTask = {
+            id : taskId,
+            name : taskName,
+            isCompleted : false
+        };
+        localStorage.setItem(taskId, JSON.stringify(newTask));
+        for(var i=0; i<addedCallbacks.length; i++){
+            var callback = addedCallbacks[i];
+            callback(newTask);
+        }
+    }
+    function toggleTask(id){
+        var task = JSON.parse(localStorage.getItem(id));
+        task.isCompleted = !task.isCompleted;
+        localStorage.setItem(id, JSON.stringify(task));
+    }
+    function addListenerForNewTask(callback){
+        addedCallbacks.push(callback);
+    }
+    return{
+        getAll : getAllTasks,
+        remove : removeTask,
+        add : addTask,
+        toggle : toggleTask,
+        addListenerForNewTask : addListenerForNewTask
+    };
 
-        })();
+})();
